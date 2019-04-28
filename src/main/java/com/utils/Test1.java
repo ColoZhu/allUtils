5a3460229e3750ac3c018c130a4cd5c107c20976
ColoZhu <611766335@qq.com>
2019-04-28 17:37:21 +0800

    ~lombok

--- src/main/java/com/utils/Test1.java
+++ src/main/java/com/utils/Test1.java
@@ -9,46 +9,17 @@ import java.util.Map;
 
 public class Test1 {
 
+    private Long type = 1 == 2 ? 7L : 8L;
+
+     
 
     @Test
-    public void test1() {
-
-        // ArrayList<String> skuCodeList = new ArrayList<>();
-        //skuCodeList.add("商品有入库记录无法更新包装规格!商品编码:" + "BNXX211" + " 新包装规格:" + "ANXX211");
-        //skuCodeList.add("商品有入库记录无法更新包装规格!商品编码:" + "BNXX211" + " 新包装规格:" + "ANXX211");
-        // skuCodeList.add("商品有入库记录无法更新包装规格!商品编码:" + "BNXX211" + " 新包装规格:" + "ANXX211");
-        //System.out.println("skuCodeList :" + skuCodeList);
-       /*  for(String item : skuCodeList) {
-             System.out.println(  item);
-         }*/
+    public void test() {
+        Test1 test1 = new Test1();
+        System.out.println("test1 :" + test1.type);
 
 
-    /*    List<String> a = new ArrayList<>();
-        a.add("1");
-        a.add("2");
-        //a.add("3");
-        for (String temp : a) {
-            if ("1".equals(temp)) {
-                a.remove(temp);
-            }
-        }
-*/
-
-        Map<String, String> skuMap = new HashMap<>();
-
-
-        for (int i = 0; i < 60000; i++) {
-            skuMap.put ("1443990001"+i, "6944804590104");
-        }
-        long currentTime1 = System.currentTimeMillis();
-
-
-        for (Map.Entry<String, String> entry : skuMap.entrySet()) {
-            String skuCode = entry.getKey();
-            String barCode = entry.getValue();
-            System.out.println(skuCode+" :" + barCode);
-        }
-        long currentTime2 = System.currentTimeMillis();
-        System.out.println("遍历map时间 :" + (currentTime2 - currentTime1));
     }
+
+
 }
